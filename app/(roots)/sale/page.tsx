04e7ee538e1a42c95c50bba13cc17abad3a112a3
Denız  Kurt <denizkurt@192.168.1.57>
2024-02-15 18:58:34 +0300

    vercel 15

---
+++ app/(roots)/sale/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SalesClient from "./salesClient";
+
+const SalePage = () => {
+  return (
+    <div className="bg-white p-4 w-full">
+      <SalesClient />
+    </div>
+  );
+};
+
+export default SalePage;
